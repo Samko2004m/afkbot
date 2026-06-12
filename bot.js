c55e219ab80485fd4976ea93093d9c2be691aad0
Samko2004m <kisssamko14@gmail.com>
2026-06-12 20:11:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,13 @@
-const bot = mineflayer.createBot({
-  host: 'normalnyserver123.aternos.me',
-  port: 36726,
-  username: 'AFKBot',
-  auth: 'offline',
-  version: false
-});
+const mineflayer = require('mineflayer');
+
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'normalnyserver123.aternos.me',
+    port: 36726,
+    username: 'AFKBot',
+    auth: 'offline'
+  });
+
   bot.on('spawn', () => {
     console.log('Bot sa pripojil');
 
