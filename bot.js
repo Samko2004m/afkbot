000bc88cd0fc043f7bc303aba3d0ef290b22d0ea
Samko2004m <kisssamko14@gmail.com>
2026-06-12 20:27:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,8 @@ console.log("NOVA VERZIA BOTA");
 const mineflayer = require('mineflayer');
 
 function createBot() {
+  console.log("VYTVARAM BOTA");
+
   const bot = mineflayer.createBot({
     host: 'normalnyserver123.aternos.me',
     port: 36726,
@@ -10,20 +12,37 @@ function createBot() {
     auth: 'offline'
   });
 
+  console.log("BOT VYTVORENY");
+
+  bot.on('login', () => {
+    console.log('LOGIN OK');
+  });
+
   bot.on('spawn', () => {
     console.log('Bot sa pripojil');
+
+    setInterval(() => {
+      bot.setControlState('jump', true);
+
+      setTimeout(() => {
+        bot.setControlState('jump', false);
+      }, 500);
+
+      bot.look(Math.random() * Math.PI * 2, 0, true);
+    }, 30000);
   });
 
   bot.on('error', (err) => {
-    console.log('ERROR:', err);
+    console.log('ERROR:', err.message);
   });
 
   bot.on('kicked', (reason) => {
     console.log('KICKED:', reason);
   });
 
-  bot.on('end', () => {
-    console.log('DISCONNECTED');
+  bot.on('end', (reason) => {
+    console.log('DISCONNECTED:', reason);
+    setTimeout(createBot, 10000);
   });
 }
 
